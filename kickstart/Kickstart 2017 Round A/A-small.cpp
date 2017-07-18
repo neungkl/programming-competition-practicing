4d8253192333e32ee029b4351f6c0cd27a591181
Kosate Limpongsa <kosatelim@gmail.com>
2017-07-18 22:42:59 +0700

    Add kickstart round A/A-small

--- kickstart/Kickstart 2017 Round A/A-small.cpp
+++ kickstart/Kickstart 2017 Round A/A-small.cpp
@@ -2,6 +2,7 @@
 
 typedef long long llint;
 int MOD = 1000000007;
+int MAX_SIZE = 1000000001;
 
 int min(int x, int y) {
   return x < y ? x : y;
@@ -13,9 +14,10 @@ void solve() {
   llint M = min(R, C);
   llint sol = 0;
   for(llint m=2; m<=M; m++) {
-    sol += 
+    sol += (R - m + 1) * (C - m + 1) * (m - 1);
+    sol %= MOD;
   }
-  printf("%lld", sol % MOD)
+  printf("%lld", sol % MOD);
 }
 
 int main() {
@@ -24,5 +26,21 @@ int main() {
   for(int i=1; i<=T; i++) {
     printf("Case #%d: ", i);
     solve();
+    printf("\n");
   }
 }
+
+/*
+===
+4
+2 4
+3 4
+4 4
+1000 500
+---
+Case #1: 3
+Case #2: 10
+Case #3: 20
+Case #4: 624937395
+===
+*/
